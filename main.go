ce4e4af962e38b7c5398b37211031d8f77ac7f85
Groovy Sky <groovymail>
2023-04-16 16:43:00 +0000

    working on

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/storage/armstorage"
+
+	"github.com/urfave/cli/v2"
 )
 
 type accessList struct {
@@ -27,50 +32,124 @@ func azureLogin() (cred *azidentity.EnvironmentCredential, err error) {
 	return cred, err
 }
 
-func getResource(cred azcore.TokenCredential) {
+func parseResourceId(resourceId string) (subscriptionId, resourceGroup, resourceProvider, resourceName string) {
+	parts := strings.Split(resourceId, "/")
+	subscriptionId = parts[2]
+	resourceGroup = parts[4]
+	resourceProvider = strings.Join(parts[6:8], "/")
+	resourceName = parts[8]
+	return subscriptionId, resourceGroup, resourceProvider, resourceName
+}
 
-	ctx := context.Background()
+func updateNetAcl(cred azcore.TokenCredential, resourceId, allowIPList, allowVNetList string) {
 
-	storageAccountsClient, err := armstorage.NewAccountsClient(subscriptionID, cred, nil)
+	subscriptionID, resourceGroupName, resourceProvider, resourceName := parseResourceId(resourceId)
 
+	switch resourceProvider {
+	case "Microsoft.Storage/storageAccounts":
+		ctx := context.Background()
+
+		storageAccountsClient, err := armstorage.NewAccountsClient(subscriptionID, cred, nil)
+
+		if err != nil {
+			panic(err)
+		}
+
+		resource, err := storageAccountsClient.GetProperties(ctx, resourceGroupName, resourceName, &armstorage.AccountsClientGetPropertiesOptions{Expand: nil})
+		if err != nil {
+			panic(err)
+		}
+
+		for _, ip := range strings.Split(allowIPList, ";") {
+			var exists bool
+			for _, ipRule := range resource.Properties.NetworkRuleSet.IPRules {
+				if *ipRule.IPAddressOrRange == ip {
+					exists = true
+					break
+				}
+
+			}
+			if !exists {
+				newRuleSet := &armstorage.IPRule{
+					Action:           &[]string{"Allow"}[0],
+					IPAddressOrRange: &[]string{ip}[0],
+				}
+				resource.Properties.NetworkRuleSet.IPRules = append(resource.Properties.NetworkRuleSet.IPRules, newRuleSet)
+			}
+		}
+
+		for _, ipRule := range resource.Properties.NetworkRuleSet.IPRules {
+			fmt.Println(*ipRule)
+		}
+
+		for _, v := range resource.Properties.NetworkRuleSet.IPRules {
+			fmt.Println(*v.IPAddressOrRange, *v.Action)
+		}
+
+		fmt.Println(*resource.Properties)
+
+		response, err := storageAccountsClient.Update(ctx, resourceGroupName, resourceName, armstorage.AccountUpdateParameters{Properties: &armstorage.AccountPropertiesUpdateParameters{NetworkRuleSet: resource.Properties.NetworkRuleSet}}, nil)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(response)
+	}
+
+}
+
+func getInputParams() (resList, ipList, vnetList string) {
+	app := &cli.App{
+		Name:                 "aznet",
+		Usage:                "CLI tool to set Azure PaaS network access",
+		EnableBashCompletion: true,
+		Action: func(c *cli.Context) error {
+			return nil
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "source",
+				Aliases:     []string{"s"},
+				Value:       "",
+				Usage:       "Sources of PaaS resources list",
+				Destination: &resList,
+				Required:    true,
+			},
+			&cli.StringFlag{
+				Name:        "ips",
+				Aliases:     []string{"i"},
+				Value:       "",
+				Usage:       "Allowed IPs list",
+				Destination: &ipList,
+			},
+			&cli.StringFlag{
+				Name:        "vnets",
+				Aliases:     []string{"v"},
+				Value:       "",
+				Usage:       "Allowed VNets list",
+				Destination: &vnetList,
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
-
-	resource, err := storageAccountsClient.GetProperties(ctx, resourceGroupName, resourceName, &armstorage.AccountsClientGetPropertiesOptions{Expand: nil})
-	if err != nil {
-		panic(err)
+	if resList == "" || (ipList == "" && vnetList == "") {
+		log.Fatal("[ERR] Not enough imput parameters")
 	}
+	return resList, ipList, vnetList
 
-	newRuleSet := &armstorage.IPRule{
-		Action:           &[]string{"Allow"}[0],
-		IPAddressOrRange: &[]string{"4.4.4.4/24"}[0],
-	}
-
-	resource.Properties.NetworkRuleSet.IPRules = append(resource.Properties.NetworkRuleSet.IPRules, newRuleSet)
-
-	for _, v := range resource.Properties.NetworkRuleSet.IPRules {
-		fmt.Println(*v.IPAddressOrRange, *v.Action)
-	}
-
-	fmt.Println(*resource.Properties)
-
-	response, err := storageAccountsClient.Update(ctx, resourceGroupName, resourceName, armstorage.AccountUpdateParameters{Properties: &armstorage.AccountPropertiesUpdateParameters{NetworkRuleSet: resource.Properties.NetworkRuleSet}}, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(response)
-
-	fmt.Println(*resource.Properties.NetworkRuleSet.DefaultAction, *resource.Properties.NetworkRuleSet.Bypass, resource.Properties.NetworkRuleSet.IPRules, resource.Properties.NetworkRuleSet.ResourceAccessRules, resource.Properties.NetworkRuleSet.VirtualNetworkRules)
 }
 
 func main() {
+
+	res, ips, vnets := getInputParams()
 
 	login, err := azureLogin()
 	if err != nil {
 		panic(err)
 	}
-	getResource(login)
+	updateNetAcl(login, res, ips, vnets)
 
 }
